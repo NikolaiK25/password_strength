98876d4efc74f4b5574efdc786a1e59283567487
NikolaiK25 <info.nikkin@gmail.com>
2025-12-01 20:53:52 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+def is_very_long(password):
+    return len(password) >= 13
+
+
+def has_digit(password):
+        return any(letter.isdigit() for letter in password)
+
+
+def has_lower_letters(password):
+        return any(letter.islower() for letter in password)
+
+
+def has_upper_letters(password):
+        return any(letter.isupper() for letter in password)
+
+
+def has_symbols(password):
+        return any(not letter.isdigit() and not letter.isalpha() for letter in password)
+
+
+password_functions = [
+    is_very_long,
+    has_digit,
+    has_lower_letters,
+    has_upper_letters,
+    has_symbols
+]
+
+password = input("Введите пароль: ")
+
+score = 0
+
+for func in password_functions:
+    if func(password):
+        score += 2
+
+
+print(f"\nРейтинг пароля:  {score}")
